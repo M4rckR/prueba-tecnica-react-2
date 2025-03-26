ac2eab723e8b3b0785e7347594e23f8f898d1fab
Marcos Daniel Romero Garcia <Marck Romero>
2025-03-26 15:47:49 -0500

    Proyecto iniciado

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form className="form">
+          <input placeholder="Avengers, Star Wars, The Matrix" type="text" />
+        </form>
+      </header>
+
+      <main>
+        Aqui iran los resoltados
+      </main>
+    </div>
+  )
+}
