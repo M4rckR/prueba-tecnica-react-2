f7af0326e8357945f23c9b0bef7764f279b08047
Marcos Daniel Romero Garcia <Marck Romero>
2025-03-28 04:36:31 -0500

    Proyecto terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
+import { useCallback, useState } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 import { useSearch } from './hooks/useSearch'
-
-
+import debounce from 'just-debounce-it'
 
 export const App = () => {
+  const [sort, SetSort] = useState(false)
   const { search, updateSearch, error}  = useSearch()
-  const { movies, getMovies, loading } = useMovies({search})
+  const { movies, getMovies, loading } = useMovies({search, sort})
   // const inputRef = useRef() no se abusa de los refs
+  
+  
+
+
+  const debounceGetMovies = useCallback(
+    debounce((search) => {
+      getMovies({ search })
+    }, 1000), // Se aumenta a 500ms para evitar spam de peticiones
+    []
+  )
   
 
   // Vale la pena repasar esta forma de obtener los datos del formulario
+  const handleChange = (event) => {
+    const newSearch = event.target.value
+    updateSearch(newSearch)
+    debounceGetMovies(newSearch) // Asegurar que se usa la versión correcta
+  }
+  
   const handleSubmit = (event) => {
     event.preventDefault()
-    getMovies()
+    getMovies({search})
   }
 
-  const handleChange = (event) => {
-    updateSearch(event.target.value)
-    // Validacion
+  const handleSort = () => {
+    SetSort(!sort)
   }
+
 
 
 
@@ -29,6 +46,7 @@ export const App = () => {
         <h1>Buscador de peliculas</h1>
         <form className="form" onSubmit={handleSubmit}>
           <input value={search} onChange={handleChange} name="search" placeholder="Avengers, Star Wars, The Matrix" type="text" />
+          <input type="checkbox" onChange={handleSort} checked={sort}/>
           <button type="submit">Buscar</button>
         </form>
         {error && <p style={{color:'red'}} className='error'>{error}</p>}
--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,31 +1,64 @@
-import { useRef, useState } from 'react'
-import { searchMovies } from '../services/movies'
+import { useCallback, useMemo, useRef, useState } from "react";
+import { searchMovies } from "../services/movies";
 
-export const useMovies = ({ search }) => {
-    const [movies, setMovies] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
-    const previousSearch = useRef(search)
+export const useMovies = ({ search, sort }) => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const [error, setError] = useState(null);
+  const previousSearch = useRef(search);
 
-    const getMovies = async() => {
+  //useMemo
+  // const getMovies = useMemo( () => {
+  //     return async ({search}) => {
+  //         if(search === previousSearch.current) return
 
-        if(search === previousSearch.current) return
-        
+  //         try {
+  //             setLoading(true)
+  //             setError(null)
+  //             previousSearch.current = search
+  //             const response = await searchMovies({search})
+  //             setMovies(response)
+  //         }
+  //         catch(e) {
+  //             setError('Error en la búsqueda de películas. Por favor intente nuevamente.', e)
+  //             setError(e.message)
+  //         }
+  //         finally {
+  //             setLoading(false)
+  //         }
+  //     }
 
-        try {
-            setLoading(true)
-            setError(null)
-            previousSearch.current = search
-            const response = await searchMovies({search})
-            setMovies(response)
-        }
-        catch(e) {
-            setError('Error en la búsqueda de películas. Por favor intente nuevamente.', e)
-            setError(e.message)
-        }
-        finally {
-            setLoading(false)
-        }
-    }
-    return { movies, getMovies, loading }
-  }
+  // } , [search])
+
+  const getMovies = useCallback(
+    async ({search}) => {
+      if (search === previousSearch.current) return;
+
+      try {
+        setLoading(true);
+        setError(null);
+        previousSearch.current = search;
+        const response = await searchMovies({ search });
+        setMovies(response);
+      } catch (e) {
+        setError(
+          "Error en la búsqueda de películas. Por favor intente nuevamente.",
+          e
+        );
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    },[search]);
+
+    
+  //Use Memo para no ejecutarlo cada rato
+  const sortedMovies = useMemo(() => {
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies;
+  }, [sort, movies]);
+
+  return { movies: sortedMovies, getMovies, loading, sortedMovies };
+};
